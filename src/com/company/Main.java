8d02a983b96778dfbf5beedf5ef0e1c9c79225cc
tomm8231 <tomm8231@stud.kea.dk>
2022-02-25 13:13:17 +0100

    Added greet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public void greet() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Indtast dit navn:");
+        String navn = sc.nextLine();
+
+        System.out.println("Hej, " + navn);
+    }
+
     public static void main(String[] args) {
+        Main obj = new Main();
+
         System.out.println("It works!");
+
+        obj.greet();
+
     }
 }
